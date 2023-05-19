99b230ab51cb5f58c992a3c1e1c23c6f4c58c4b7
Trevor Erwin <37050317+trevorerwin@users.noreply.github.com>
2023-05-19 10:52:23 -0400

    init react app

--- controllers/svg_tag.controller.js
+++ controllers/svg_tag.controller.js
@@ -1,48 +1,42 @@
-const router = require("express").Router();
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const db = require("../db.js");
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../db.js');
 
 // http://localhost:4000/svg_tag/display-by-tag/:svgTag
-router.get("/display-by-tag/:svgTag", (req, res) => {
-    try {
-        const svgTag = req.params.svgTag;
+router.get('/display-by-tag/:svgTag', (req, res) => {
+  try {
+    const svgTag = req.params.svgTag;
 
-        const sql = `SELECT * FROM gomot1_upright_svghunter.SVG WHERE id IN (SELECT svgId FROM gomot1_upright_svghunter.SVG_Tags WHERE svgTag = ?)`;
-        const values = [svgTag];
+    const sql = `SELECT * FROM gomot1_upright_svghunter.SVG WHERE id IN (SELECT svgId FROM gomot1_upright_svghunter.SVG_Tags WHERE svgTag = ?)`;
+    const values = [svgTag];
 
-        db.query(sql, values, (error, results) => {
-            if (error) {
-                console.error("Error retrieving SVGs by tag: ", error);
-                res.status(500).json({ message: error.message });
-            } else {
-                res.status(200).json({ results: results });
-            }
-        });
-    } catch (error) {
-        console.error("Error retrieving SVGs by tag: ", error);
-        res.status(500).json({ message: "Error retrieving SVGs by tag" });
-    }
+    db.query(sql, values, (error, results) => {
+      if (error) {
+        console.error('Error retrieving SVGs by tag: ', error);
+        res.status(500).json({ message: error.message });
+      } else {
+        res.status(200).json({ results: results });
+      }
+    });
+  } catch (error) {
+    console.error('Error retrieving SVGs by tag: ', error);
+    res.status(500).json({ message: 'Error retrieving SVGs by tag' });
+  }
 });
-
-
 
 // http://localhost:4000/svg_tag/display-all
-router.get("/display-all", (req, res) => {
-    try {
-        let sql = `SELECT * FROM gomot1_upright_svghunter.SVG_Tags`;
+router.get('/display-all', (req, res) => {
+  try {
+    let sql = `SELECT * FROM gomot1_upright_svghunter.SVG_Tags`;
 
-        db.query(sql, (error, results) => {
-            if (error) throw error;
-            res.json({ results: results });
-        });
-    } catch (error) {
-        res.json({ message: error.message });
-    }
+    db.query(sql, (error, results) => {
+      if (error) throw error;
+      res.json({ results: results });
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 });
-
-
-
-
 
 module.exports = router;
