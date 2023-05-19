2bd26efff7defa017c1a63de6b711e8e8af7dd45
SimonSoccer <128168641+SimonSoccer@users.noreply.github.com>
2023-05-19 16:23:54 -0400

    added contact code

--- client/svg-hunter-app/src/App.jsx
+++ client/svg-hunter-app/src/App.jsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from "./Landing/Contact";
 
 function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1></h1>
+            <Contact />
+        </div>
+    );
 }
 
 export default App;
