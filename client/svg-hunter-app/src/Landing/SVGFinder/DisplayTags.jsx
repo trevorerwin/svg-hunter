96318610c3a49dcf9b471eb7a73e661414d93306
Ashlee Lindstrom <ashm0428@gmail.com>
2023-05-22 15:00:30 -0600

    Handles tags correctly to display them - not displaying yet

--- client/svg-hunter-app/src/Landing/SVGFinder/DisplayTags.jsx
+++ client/svg-hunter-app/src/Landing/SVGFinder/DisplayTags.jsx
@@ -4,6 +4,34 @@ const DisplayTags = (props) => {
     const [tagArray, setTagArray] = useState([]);
 
 
+    function processStrings(dataArray) {
+        // Step 1: Remove hyphens from the start of a word
+        dataArray = dataArray.map(tag => tag.replace(/^-/, ''));
+        
+        // Step 2: Remove empty strings from the array
+        dataArray = dataArray.filter(tag => tag !== '');
+        
+        // Step 3: Count the number of times each string occurs in the array
+        const counts = {};
+        dataArray.forEach(tag => {
+          const lowercaseTag = tag.toLowerCase();
+          counts[lowercaseTag] = (counts[lowercaseTag] || 0) + 1;
+        });
+
+        
+        // Step 4: If a string occurs 10 or more times, add it to a new array
+        const newTagArray = dataArray.filter(tag => counts[tag.toLowerCase()] >= 10);
+
+        const lowerCaseTagArray = newTagArray.map(tag => tag.toLowerCase());
+        
+        // Step 5: Remove duplicate strings from the new array
+        const uniqueTags = [...new Set(lowerCaseTagArray)];
+        
+        // Step 6: Sort the strings in the new array alphabetically
+        uniqueTags.sort();
+        
+        return uniqueTags;
+      }
 
     async function getAllTags() {
         let url = `http://localhost:4000/svg_tag/display-all`;
@@ -14,9 +42,12 @@ const DisplayTags = (props) => {
         try {
             const response = await fetch(url, requestOptions);
             const data = await response.json();
-            // const uniqueData = Array.from(new Set(data))
-            // console.log(uniqueData)
-            setTagArray(data.results)
+            const tagData = data.results.map((tag) => tag.svgTag.trim());
+            
+            const tagDataArray = processStrings(tagData);
+            console.log(tagDataArray);
+            // setTagArray(tagDataArray);
+            // console.log("Tag Array", tagArray);
 
         } catch (error) {
             console.error(error.message);
@@ -28,18 +59,15 @@ const DisplayTags = (props) => {
         <>
         <div className="overflow-scroll" style={{height: "70vh", border: "solid", marginRight: "30px"}}>
             <h5 style={{textAlign: "center", marginTop: "10px", marginBottom: "10px"}}>TAGS</h5>
-            console.log({tagArray});
-            {/* {tagArray.map((tag, index) => (
-                <option key={index} value={tag.svgTag}>{tag.svgTag}</option>
-            ))} */}
-            {/* //!Possible way of removing duplicates */}
-        {/* {tagArray.map(tags =>
-            [...new Set(tags)].map((tag, index) => (
-            <option key={index} value={tag.id}>{tag.svgTag}</option>
-        )))} */}
+            {/* {tagArray.forEach(tag => {
+                <p>{tag}</p>
+            })} */}
+
         </div>
         </>
      );
 }
  
-export default DisplayTags;
+
+
+export default DisplayTags
