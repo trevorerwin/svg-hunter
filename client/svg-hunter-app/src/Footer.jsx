f29fcd5724351161874384af9633efc978f3d473
Judith Tuyisingize <judithtuyisingize@Judiths-MacBook-Pro.local>
2023-05-26 15:25:36 -0400

    added login/signup/forgot password styling

--- client/svg-hunter-app/src/Footer.jsx
+++ client/svg-hunter-app/src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = (props) => {
   return (
     <>
-      <footer className='text-center p-4'>
+      <footer className="text-center p-4">
         <h6>© 2014-2016 SVGHunter.com by MichelleMyBelleCreations</h6>
       </footer>
     </>
