46d348abf29ef2ce4403d708b29978423ede3a75
Trevor Erwin <37050317+trevorerwin@users.noreply.github.com>
2023-05-26 11:48:09 -0400

    Adding create customer endpoint

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -3,54 +3,40 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const domain = 'http://localhost:3000';
 const router = require('express').Router();
 const db = require('../db.js');
+const fetchUserID = require('../middleware/fetch-user-id.js');
 
-router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: 'price_1NAd4JBbFZD5Ir3VadsttQAZ',
-        quantity: 1,
-      },
-    ],
-    mode: 'subscription',
-    success_url: `${domain}/success.html`,
-    cancel_url: `${domain}/cancel.html`,
-  });
-
-  res.redirect(303, session.url);
-});
-
-router.get('/subscription/status/:id', async (req, res) => {
+router.post('/create-customer', fetchUserID, async (req, res) => {
   try {
-    const { id } = req.params;
+    console.log(`userId: ${req.user.id}`);
+    const { email, paymentMethod } = req.body;
 
-    const sql = `SELECT StripeID FROM gomot1_upright_svghunter.sitelok WHERE id = ?`;
+    // Create customer
+    const customer = await stripe.customers.create({
+      email,
+      payment_method: paymentMethod,
+      invoice_settings: {
+        default_payment_method: paymentMethod,
+      },
+    });
 
-    db.query(sql, id, (error, results) => {
+    console.log(customer);
+
+    const userID = req.user.id;
+    const customerID = customer.id;
+
+    const sql = `UPDATE gomot1_upright_svghunter.sitelok SET StripeID = ? WHERE id = ?`;
+
+    db.query(sql, [customerID, userID], (error, results) => {
       if (error) {
-        console.error('Error retrieving subscription status:', error);
-        return res.status(500).json({ message: error.message });
+        console.error('Error updating user: ', error);
+        res.status(500).json({ message: error.message });
+      } else {
+        res.status(200).json({ results: results });
       }
-
-      if (results.length === 0 || results[0].StripeID === null) {
-        return res.status(404).json({ message: 'User not found or subscription not active' });
-      }
-
-      const stripeID = results[0].StripeID;
-      console.log(stripeID);
-
-      stripe.subscriptions.retrieve(stripeID, (error, subscription) => {
-        if (error) {
-          console.error('Error retrieving subscription status:', error);
-          return res.status(500).json({ message: error.message });
-        }
-
-        res.status(200).json({ subscription });
-      });
     });
   } catch (error) {
-    console.error('Error retrieving user subscriptions:', error);
-    res.status(500).json({ message: 'Error retrieving user subscriptions' });
+    console.error('Error creating customer:', error);
+    res.status(500).json({ error: 'Failed to create customer' });
   }
 });
 
---
+++ middleware/fetch-user-id.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const db = require('../db.js');
+
+const fetchUserID = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, 'HUNTERKILL');
+    const sql = `SELECT * FROM gomot1_upright_svghunter.sitelok WHERE id = ?`;
+
+    db.query(sql, [decodedToken.id], (error, results) => {
+      if (error) {
+        console.error('Error retrieving user: ', error);
+        res.status(500).json({ message: error.message });
+      }
+
+      if (results.length === 0) {
+        throw Error('User was not found');
+      }
+
+      req.user = results[0];
+      next();
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+module.exports = fetchUserID;
