039df9ca06625edadc782b2e6e946d128c2abbbf
Trevor Erwin <37050317+trevorerwin@users.noreply.github.com>
2023-05-27 12:57:35 -0400

    Changing endpoint to create user and sub simultaneously

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -5,62 +5,40 @@ const router = require('express').Router();
 const db = require('../db.js');
 const fetchUserID = require('../middleware/fetch-user-id.js');
 
+// Creates a Stripe customer and subscribes them to a subscription plan
 router.post('/create-customer', fetchUserID, async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, paymentMethod, priceID } = req.body;
 
-    // Create customer
+    // Create customer and subscription in Stripe
     const customer = await stripe.customers.create({
       email: email,
+      payment_method: paymentMethod,
+      invoice_settings: {
+        default_payment_method: paymentMethod,
+      },
+      items: [{ price: priceID }],
     });
 
     const userID = req.user.id;
     const customerID = customer.id;
+    const subscription = customer.subscriptions.data[0];
+    const updatedUsergroups = `${process.env.SECRET}:${subscription.id}`;
 
-    const sql = `UPDATE gomot1_upright_svghunter.sitelok SET StripeID = ? WHERE id = ?`;
+    const sql = `UPDATE gomot1_upright_svghunter.sitelok SET StripeID = ?, Usergroups = ? WHERE id = ?`;
 
-    db.query(sql, [customerID, userID], (error, results) => {
-      if (error) {
-        console.error('Error updating user: ', error);
-        res.status(500).json({ message: error.message });
+    db.query(sql, [customerID, updatedUsergroups, userID], (err, result) => {
+      if (err) {
+        console.error('Error updating customer:', err);
+        res.status(500).json({ message: err.message });
       } else {
-        res.status(200).json({ results: results });
+        const updatedUser = { ...req.user, usergroup: updatedUsergroups };
+        res.status(200).json({ message: 'Customer created successfully', updatedUser });
       }
     });
   } catch (error) {
     console.error('Error creating customer:', error);
     res.status(500).json({ error: 'Failed to create customer' });
-  }
-});
-
-router.post('/create-subscription', fetchUserID, async (req, res) => {
-  try {
-    const { priceID } = req.body;
-    const userID = req.user.id;
-    const stripeID = req.user.StripeID;
-
-    // Create subscription
-    const subscription = await stripe.subscriptions.create({
-      customer: stripeID,
-      items: [{ price: priceID }],
-      expand: ['latest_invoice.payment_intent'],
-    });
-
-    const sql = `UPDATE gomot1_upright_svghunter.sitelok SET Usergroups = ? WHERE id = ?`;
-    const updatedUsergroups = `${process.env.SECRET}:${subscription.id}`;
-
-    db.query(sql, [updatedUsergroups, userID], (error, results) => {
-      if (error) {
-        console.error('Error updating user: ', error);
-        res.status(500).json({ message: error.message });
-      } else {
-        const updatedUser = { ...req.user, usergroup: updatedUsergroups };
-        res.status(200).json({ user: updatedUser });
-      }
-    });
-  } catch (error) {
-    console.error('Error creating subscription:', error);
-    res.status(500).json({ error: 'Failed to create subscription' });
   }
 });
 
