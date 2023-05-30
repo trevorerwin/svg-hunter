c2b2d33919317b7cf8bfd324407749988f4ef425
Trevor Erwin <37050317+trevorerwin@users.noreply.github.com>
2023-05-30 11:04:44 -0400

    Implementing frontend logic

--- client/svg-hunter-app/src/Landing/SVGFinder/SVGFinder.jsx
+++ client/svg-hunter-app/src/Landing/SVGFinder/SVGFinder.jsx
@@ -2,68 +2,136 @@
 import DisplaySVG from './DisplaySVG';
 import DisplayTags from './DisplayTags';
 import { Col, Container, Input, Row } from 'reactstrap';
-import './SVG-Styles.css'
-import React, { useState } from 'react';
+import './SVG-Styles.css';
+import React, { useState, useEffect } from 'react';
 
 const SVGFinder = (props) => {
-    const [selectedTags, setSelectedTags] = useState("");
-    const [totalTagArray, setTotalTagArray] = useState([]);
-    const [search, setSearch] = useState("");
-    const [searchList, setSearchList] = useState([]);
-    const [chosenSearchTag, setChosenSearchTag] = useState("");
+  const [selectedTags, setSelectedTags] = useState('');
+  const [totalTagArray, setTotalTagArray] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchList, setSearchList] = useState([]);
+  const [chosenSearchTag, setChosenSearchTag] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
+  const [subscribed, setSubscribed] = useState(false);
 
-    function handleSelect(item) {
-      setChosenSearchTag(item);
-    }
-
-    async function displayByInput(e) {
-      setSearch(e.target.value)
-      let searchTagName = totalTagArray.filter((searchTag) => searchTag.includes(e.target.value.toLowerCase()));
-      setSearchList(searchTagName);
-      console.log("Search List", searchList)
+  function handleSelect(item) {
+    setChosenSearchTag(item);
   }
 
-  return (
-    <>
-    <div className='svg-finder-page'>
-    <Container fluid className='svg-search-bar-container' >
-    <Row className='w-100'>
-            <Col lg="2">
-            </Col>
+  async function displayByInput(e) {
+    setSearch(e.target.value);
+    let searchTagName = totalTagArray.filter((searchTag) => searchTag.includes(e.target.value.toLowerCase()));
+    setSearchList(searchTagName);
+    console.log('Search List', searchList);
+  }
 
-            <Col lg="7" className='svg-search-bar' >
-                <Input onChange={displayByInput} className='svg-search-input' placeholder="Search" />
-            </Col>
-                
-            <Col lg="1" className='svg-search-btn' >
-                <button className='search-btn'>Search</button>
-            </Col>
+  async function checkSubscriptionStatus() {
+    const token = localStorage.getItem('token');
+    let url = `http://localhost:4000/user/get-user`;
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Authorization', token);
 
-            <Col lg="2">
-            </Col>
-        </Row>
-    </Container>
+    const requestOptions = {
+      headers: myHeaders,
+      method: 'GET',
+    };
 
-    <Container fluid className ="SVG-finder-container" > 
-        
+    try {
+      const response = await fetch(url, requestOptions);
+      const data = await response.json();
+      console.log(data.user.Usergroups);
+      if (data.user.Usergroups.includes(':')) {
+        setSubscribed(true);
+      } else {
+        setSubscribed(false);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
-        <Row>
-          <Col lg='2' className='tag-display-column' >
-            <DisplayTags selectedTags={selectedTags} setSelectedTags={setSelectedTags} totalTagArray={totalTagArray} setTotalTagArray={setTotalTagArray}/>
-          </Col>
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
 
-          <Col lg='9' className='svg-display-column'>
-            <DisplaySVG selectedTags={selectedTags} chosenSearchTag={chosenSearchTag}/>
+    checkSubscriptionStatus();
+  }, []);
 
-          </Col>
-
-          <Col lg="1">
-          </Col>
-        </Row>
-      </Container>
+  if (!authenticated) {
+    return (
+      <div className='login-prompt'>
+        <h1 className='login-prompt-header'>Access Restricted</h1>
+        <p className='login-prompt-text'>Please log in to access the SVGFinder and explore our collection of SVGs.</p>
+        <button
+          className='login-prompt-button'
+          onClick={() => {
+            /* Handle login redirect */
+          }}
+        >
+          Log In
+        </button>
       </div>
-    </>
-  );
+    );
+  } else if (!subscribed) {
+    return (
+      <div className='login-prompt'>
+        <h1 className='login-prompt-header'>Access Restricted</h1>
+        <p className='login-prompt-text'>
+          You must have a paying subscription in order to access our SVG Finder. Please click the button below to pay and access our application through{' '}
+          <span className='login-stripe-text'>Stripe</span>
+        </p>
+        <button
+          className='login-prompt-button'
+          onClick={() => {
+            /* Handle subscription redirect */
+          }}
+        >
+          Subscribe
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className='svg-finder-page'>
+          <Container fluid className='svg-search-bar-container'>
+            <Row className='w-100'>
+              <Col lg='2'></Col>
+
+              <Col lg='7' className='svg-search-bar'>
+                <Input onChange={displayByInput} className='svg-search-input' placeholder='Search' />
+              </Col>
+
+              <Col lg='1' className='svg-search-btn'>
+                <button className='search-btn'>Search</button>
+              </Col>
+
+              <Col lg='2'></Col>
+            </Row>
+          </Container>
+
+          <Container fluid className='SVG-finder-container'>
+            <Row>
+              <Col lg='2' className='tag-display-column'>
+                <DisplayTags selectedTags={selectedTags} setSelectedTags={setSelectedTags} totalTagArray={totalTagArray} setTotalTagArray={setTotalTagArray} />
+              </Col>
+
+              <Col lg='9' className='svg-display-column'>
+                <DisplaySVG selectedTags={selectedTags} chosenSearchTag={chosenSearchTag} />
+              </Col>
+
+              <Col lg='1'></Col>
+            </Row>
+          </Container>
+        </div>
+      </>
+    );
+  }
 };
 
 export default SVGFinder;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,7 +23,7 @@ router.post('/signup', async (req, res) => {
     const { Username, Email, Passphrase, Name } = req.body;
     const hashedPassphrase = await bcrypt.hash(Passphrase, 10);
     const sql = 'INSERT INTO sitelok (Username, Email, Passphrase, Name, Usergroups) VALUES (?, ?, ?, ?, ?)';
-    const values = [Username, Email, hashedPassphrase, Name, `${process.env.SECRET}`];
+    const values = [Username, Email, hashedPassphrase, Name, `${process.env.USER_GROUP}`];
     db.query(sql, values, (error) => {
       if (error) {
         console.error('Error signing up: ', error);
@@ -37,7 +37,6 @@ router.post('/signup', async (req, res) => {
     console.error('Error signing up: ', error);
     res.status(500).json({ message: 'Error signing up' });
   }
-
 });
 
 // http://localhost:4000/user/login
@@ -50,91 +49,109 @@ router.post('/login', (req, res) => {
       return res.status(400).json({ message: 'Username and passphrase are required' });
     }
 
-        const sql = `SELECT * FROM sitelok WHERE Username = '${Username}'`;
-        db.query(sql, async (error, userArray) => {
-            if (error) {
-                console.error("Error logging in: ", error);
-                return res.status(500).json({ message: error.message });
-            }
+    const sql = `SELECT * FROM sitelok WHERE Username = '${Username}'`;
+    db.query(sql, async (error, userArray) => {
+      if (error) {
+        console.error('Error logging in: ', error);
+        return res.status(500).json({ message: error.message });
+      }
 
-            if (userArray.length === 0) {
-                return res.status(401).json({ message: "pooped my pants" });
-            }
+      if (userArray.length === 0) {
+        return res.status(401).json({ message: 'pooped my pants' });
+      }
 
-            const user = userArray[0];
-            const isPassphraseValid = await bcrypt.compare(
-                Passphrase,
-                user.Passphrase
-            );
-            console.log(user);
+      const user = userArray[0];
+      const isPassphraseValid = await bcrypt.compare(Passphrase, user.Passphrase);
+      console.log(user);
 
-            if (!isPassphraseValid) {
-                return res.json({ message: "Invalid credentials" });
-            }
-            // TODO make secret web token secret in dotenv file
-            // Generate a JWT token
-            const token = jwt.sign({ Username }, `${process.env.SECRET}`);
-            // Send the token in the response
-            res.status(200).json({ message: "Login successful", token });
-        });
-    } catch (error) {
-        console.error("Error logging in: ", error);
+      if (!isPassphraseValid) {
+        return res.json({ message: 'Invalid credentials' });
+      }
+      // TODO make secret web token secret in dotenv file
+      // Generate a JWT token
+      const token = jwt.sign({ Username }, `${process.env.SECRET}`);
+      // Send the token in the response
+      res.status(200).json({ message: 'Login successful', token });
+    });
+  } catch (error) {
+    console.error('Error logging in: ', error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/get-user', async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, `${process.env.SECRET}`);
+    const sql = `SELECT * FROM sitelok WHERE Username = ?`;
+
+    db.query(sql, [decodedToken.Username], (error, results) => {
+      if (error) {
+        console.error('Error retrieving user: ', error);
         res.status(500).json({ message: error.message });
-    }
+      }
+
+      if (results.length === 0) {
+        throw Error('User was not found');
+      }
+
+      res.status(200).json({ user: results[0] });
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 });
 
 // http://localhost:4000/user/send-email
-router.post("/send-email", async (req, res) => {
-    try {
-        const { name, email, subject, message } = req.body;
+router.post('/send-email', async (req, res) => {
+  try {
+    const { name, email, subject, message } = req.body;
 
-        if (!name || !email || !subject || !message) {
-            return res.status(400).json({ message: "Missing required fields" });
-        }
-
-        let transporter = nodemailer.createTransport({
-            host: "mail.svghunter.com",
-            port: 465,
-            secure: true, // true for 465, false for other ports
-            auth: {
-                user: "support-demo@svghunter.com", // generated ethereal user
-                pass: "uprightstudent", // generated ethereal password
-            },
-        });
-
-        const mailOptions = {
-            from: email, // Use the user-entered email as the sender's email
-            to: "upright.lv.team@gmail.com", // Replace with the email address where you want to receive the emails
-            subject: subject,
-            text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
-        };
-
-        const mailOptions2 = {
-            from: "support-demo@svghunter.com",
-            to: email,
-            subject: subject,
-            text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error("Error sending email:", error);
-                return res.status(500).json({ message: "Error sending email" });
-            }
-            transporter.sendMail(mailOptions2, (error, info) => {
-                if (error) {
-                    console.error("Error sending email:", error);
-                    return res
-                        .status(500)
-                        .json({ message: "Error sending email" });
-                }
-            });
-            res.status(200).json({ message: "Email sent" });
-        });
-    } catch (error) {
-        console.error("Error sending email:", error);
-        res.status(500).json({ message: "Error sending email" });
+    if (!name || !email || !subject || !message) {
+      return res.status(400).json({ message: 'Missing required fields' });
     }
+
+    let transporter = nodemailer.createTransport({
+      host: 'mail.svghunter.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'support-demo@svghunter.com', // generated ethereal user
+        pass: 'uprightstudent', // generated ethereal password
+      },
+    });
+
+    const mailOptions = {
+      from: email, // Use the user-entered email as the sender's email
+      to: 'upright.lv.team@gmail.com', // Replace with the email address where you want to receive the emails
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    const mailOptions2 = {
+      from: 'support-demo@svghunter.com',
+      to: email,
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).json({ message: 'Error sending email' });
+      }
+      transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) {
+          console.error('Error sending email:', error);
+          return res.status(500).json({ message: 'Error sending email' });
+        }
+      });
+      res.status(200).json({ message: 'Email sent' });
+    });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Error sending email' });
+  }
 });
 
 // http://localhost:4000/user/reset-password
@@ -224,16 +241,11 @@ router.patch('/update-password', async (req, res) => {
       const user = userArray[0];
 
       // Check if the existing password matches the stored password
-      const isExistingPasswordValid = await bcrypt.compare(
-           Passphrase,
-           user.Passphrase
-       );
+      const isExistingPasswordValid = await bcrypt.compare(Passphrase, user.Passphrase);
 
-       if (!isExistingPasswordValid) {
-           return res
-               .status(401)
-               .json({ message: "Invalid existing password" });
-       }
+      if (!isExistingPasswordValid) {
+        return res.status(401).json({ message: 'Invalid existing password' });
+      }
 
       // Hash the new password
       const hashedNewPassword = await bcrypt.hash(NewPassword, 10);
