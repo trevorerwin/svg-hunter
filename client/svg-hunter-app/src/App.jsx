e5f1d5551a5b23a4daf328231c1d5a7f63ac102e
Judith Tuyisingize <judithtuyisingize@Judiths-MacBook-Pro.local>
2023-05-30 10:31:07 -0400

    deleted help guide

--- client/svg-hunter-app/src/App.jsx
+++ client/svg-hunter-app/src/App.jsx
@@ -1,23 +1,22 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import NavBar from './NavBar';
-import AuthToggle from './Auth/AuthToggle';
-import { Route, Routes } from 'react-router-dom';
-import Contact from './Landing/Contact';
-import Home from './Landing/Home';
-import SVGFinder from './Landing/SVGFinder/SVGFinder';
-import HelpGuide from './Landing/HelpGuide';
-import AuthContext from './Auth/AuthContext';
-import ForgotPassword from './Auth/ForgotPassword';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./NavBar";
+import AuthToggle from "./Auth/AuthToggle";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./Landing/Contact";
+import Home from "./Landing/Home";
+import SVGFinder from "./Landing/SVGFinder/SVGFinder";
+import AuthContext from "./Auth/AuthContext";
+import ForgotPassword from "./Auth/ForgotPassword";
 
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setToken(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
     }
   }, []);
 
@@ -25,7 +24,7 @@ function App() {
     //update the state of toke
     setToken(newToken);
     // update our local storage
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
   }
   return (
     <div>
@@ -33,12 +32,11 @@ function App() {
       <NavBar />
       <AuthContext.Provider value={{ updateToken, token }}>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/auth' element={<AuthToggle />} />
-          <Route path='/svg-hunter' element={<SVGFinder />} />
-          <Route path='/help-guide' element={<HelpGuide />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/forgotPassword' element={<ForgotPassword />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<AuthToggle />} />
+          <Route path="/svg-hunter" element={<SVGFinder />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
         </Routes>
       </AuthContext.Provider>
       <Footer />
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,13 +23,13 @@ router.post('/signup', async (req, res) => {
     const { Username, Email, Passphrase, Name } = req.body;
     const hashedPassphrase = await bcrypt.hash(Passphrase, 10);
     const sql = 'INSERT INTO sitelok (Username, Email, Passphrase, Name, Usergroups) VALUES (?, ?, ?, ?, ?)';
-    const values = [Username, Email, hashedPassphrase, Name, `${process.env.SECRET}`];
+    const values = [Username, Email, hashedPassphrase, Name, `${process.env.USER_GROUP}`];
     db.query(sql, values, (error) => {
       if (error) {
         console.error('Error signing up: ', error);
         res.status(500).json({ message: error.message });
       } else {
-        const token = jwt.sign({ Username, Email }, `${process.env.SECRET}`);
+        const token = jwt.sign({ Username, Email }, `${process.env.USER_GROUP}`);
         res.status(200).json({ message: 'new user created', token });
       }
     });
@@ -50,91 +50,91 @@ router.post('/login', (req, res) => {
       return res.status(400).json({ message: 'Username and passphrase are required' });
     }
 
-        const sql = `SELECT * FROM sitelok WHERE Username = '${Username}'`;
-        db.query(sql, async (error, userArray) => {
-            if (error) {
-                console.error("Error logging in: ", error);
-                return res.status(500).json({ message: error.message });
-            }
-
-            if (userArray.length === 0) {
-                return res.status(401).json({ message: "pooped my pants" });
-            }
-
-            const user = userArray[0];
-            const isPassphraseValid = await bcrypt.compare(
-                Passphrase,
-                user.Passphrase
-            );
-            console.log(user);
-
-            if (!isPassphraseValid) {
-                return res.json({ message: "Invalid credentials" });
-            }
-            // TODO make secret web token secret in dotenv file
-            // Generate a JWT token
-            const token = jwt.sign({ Username }, `${process.env.SECRET}`);
-            // Send the token in the response
-            res.status(200).json({ message: "Login successful", token });
-        });
-    } catch (error) {
+    const sql = `SELECT * FROM sitelok WHERE Username = '${Username}'`;
+    db.query(sql, async (error, userArray) => {
+      if (error) {
         console.error("Error logging in: ", error);
-        res.status(500).json({ message: error.message });
-    }
+        return res.status(500).json({ message: error.message });
+      }
+
+      if (userArray.length === 0) {
+        return res.status(401).json({ message: "pooped my pants" });
+      }
+
+      const user = userArray[0];
+      const isPassphraseValid = await bcrypt.compare(
+        Passphrase,
+        user.Passphrase
+      );
+      console.log(user);
+
+      if (!isPassphraseValid) {
+        return res.json({ message: "Invalid credentials" });
+      }
+      // TODO make secret web token secret in dotenv file
+      // Generate a JWT token
+      const token = jwt.sign({ Username }, `${process.env.SECRET}`);
+      // Send the token in the response
+      res.status(200).json({ message: "Login successful", token });
+    });
+  } catch (error) {
+    console.error("Error logging in: ", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // http://localhost:4000/user/send-email
 router.post("/send-email", async (req, res) => {
-    try {
-        const { name, email, subject, message } = req.body;
+  try {
+    const { name, email, subject, message } = req.body;
 
-        if (!name || !email || !subject || !message) {
-            return res.status(400).json({ message: "Missing required fields" });
-        }
-
-        let transporter = nodemailer.createTransport({
-            host: "mail.svghunter.com",
-            port: 465,
-            secure: true, // true for 465, false for other ports
-            auth: {
-                user: "support-demo@svghunter.com", // generated ethereal user
-                pass: "uprightstudent", // generated ethereal password
-            },
-        });
-
-        const mailOptions = {
-            from: email, // Use the user-entered email as the sender's email
-            to: "upright.lv.team@gmail.com", // Replace with the email address where you want to receive the emails
-            subject: subject,
-            text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
-        };
-
-        const mailOptions2 = {
-            from: "support-demo@svghunter.com",
-            to: email,
-            subject: subject,
-            text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error("Error sending email:", error);
-                return res.status(500).json({ message: "Error sending email" });
-            }
-            transporter.sendMail(mailOptions2, (error, info) => {
-                if (error) {
-                    console.error("Error sending email:", error);
-                    return res
-                        .status(500)
-                        .json({ message: "Error sending email" });
-                }
-            });
-            res.status(200).json({ message: "Email sent" });
-        });
-    } catch (error) {
-        console.error("Error sending email:", error);
-        res.status(500).json({ message: "Error sending email" });
+    if (!name || !email || !subject || !message) {
+      return res.status(400).json({ message: "Missing required fields" });
     }
+
+    let transporter = nodemailer.createTransport({
+      host: "mail.svghunter.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "support-demo@svghunter.com", // generated ethereal user
+        pass: "uprightstudent", // generated ethereal password
+      },
+    });
+
+    const mailOptions = {
+      from: email, // Use the user-entered email as the sender's email
+      to: "upright.lv.team@gmail.com", // Replace with the email address where you want to receive the emails
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    const mailOptions2 = {
+      from: "support-demo@svghunter.com",
+      to: email,
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        return res.status(500).json({ message: "Error sending email" });
+      }
+      transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) {
+          console.error("Error sending email:", error);
+          return res
+            .status(500)
+            .json({ message: "Error sending email" });
+        }
+      });
+      res.status(200).json({ message: "Email sent" });
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Error sending email" });
+  }
 });
 
 // http://localhost:4000/user/reset-password
@@ -225,15 +225,15 @@ router.patch('/update-password', async (req, res) => {
 
       // Check if the existing password matches the stored password
       const isExistingPasswordValid = await bcrypt.compare(
-           Passphrase,
-           user.Passphrase
-       );
+        Passphrase,
+        user.Passphrase
+      );
 
-       if (!isExistingPasswordValid) {
-           return res
-               .status(401)
-               .json({ message: "Invalid existing password" });
-       }
+      if (!isExistingPasswordValid) {
+        return res
+          .status(401)
+          .json({ message: "Invalid existing password" });
+      }
 
       // Hash the new password
       const hashedNewPassword = await bcrypt.hash(NewPassword, 10);
