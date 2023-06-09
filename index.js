f2b98cd667284df9a23343d6024c1a69ae59d243
JordytheHuman <Jordankaufman27@gmail.com>
2023-06-09 12:02:08 -0400

    deploying

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const userController = require('../svg-hunter/controllers/user.controller');
-const svgController = require('../svg-hunter/controllers/svg.controller');
-const svg_tagController = require('../svg-hunter/controllers/svg_tag.controller');
-const stripeController = require('../svg-hunter/controllers/stripe.controller');
-const checkoutController = require('../svg-hunter/controllers/checkout.controller');
+const userController = require('./controllers/user.controller');
+const svgController = require('./controllers/svg.controller');
+const svg_tagController = require('./controllers/svg_tag.controller');
+const stripeController = require('./controllers/stripe.controller');
+const checkoutController = require('./controllers/checkout.controller');
 
 app.use(cors());
 app.use('/checkout', checkoutController);
